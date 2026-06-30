734c7f973ad77e3cdff81afd99f1b02acf242f1e
Dgony1mal <gamesgames4987@gmail.com>
2026-06-30 11:24:33 +1000

    Add grid drawing

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import tkinter as tk
 
+# Размер сетки
+ROWS = 30
+COLS = 30
+CELL_SIZE = 20
+
 class PathfinderApp:
-
     def __init__(self):
-
         self.root = tk.Tk()
-
         self.root.title("Визуализатор алгоритмов поиска пути")
 
-        self.root.geometry("900x700")
+        width = COLS * CELL_SIZE
+        height = ROWS * CELL_SIZE
+
+        self.canvas = tk.Canvas(
+            self.root,
+            width=width,
+            height=height,
+            bg="white"
+        )
+
+        self.canvas.pack(padx=10, pady=10)
+
+        self.draw_grid()
+
+    def draw_grid(self):
+        """Рисование сетки"""
+
+        for row in range(ROWS):
+            for col in range(COLS):
+
+                x1 = col * CELL_SIZE
+                y1 = row * CELL_SIZE
+
+                x2 = x1 + CELL_SIZE
+                y2 = y1 + CELL_SIZE
+
+                self.canvas.create_rectangle(
+                    x1,
+                    y1,
+                    x2,
+                    y2,
+                    outline="lightgray"
+                )
 
     def run(self):
         self.root.mainloop()
